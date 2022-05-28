602b79b983988a06d0efc0d87074d9ef7e153e28
Panos Antonellos <antonps021@gmail.com>
2022-05-28 15:26:21 +0300

    [Frontend] set up images

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,12 +1,14 @@
 import { About, Footer } from './container';
+import { images } from './constants';
 
 import './App.scss';
 
 const App = () => {
   return (
-    <div>
+    <div className='app'>
       <About />
       <Footer />
+      <img src={images.logo} alt='logo' />
     </div>
   );
 };
