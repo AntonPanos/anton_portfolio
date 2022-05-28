f6c71dc61e31acc8703f7fa4b1693d18e0c23a54
Panos Antonellos <antonps021@gmail.com>
2022-05-28 14:54:16 +0300

    [Fontend & Backend] init files & folders

---
+++ frontend_react/src/App.js
@@ -0,0 +1,14 @@
+import { About, Footer } from './container';
+
+import './App.scss';
+
+const App = () => {
+  return (
+    <div>
+      <About />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
